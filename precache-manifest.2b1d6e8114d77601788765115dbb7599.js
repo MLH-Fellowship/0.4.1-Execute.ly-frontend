caf4cc582c9ebe15f3e2e715a5b0257b99df1394
Ankit Tiwari <ankitt255@gmail.com>
2020-06-05 12:00:38 +0530

    Updates

---
+++ precache-manifest.2b1d6e8114d77601788765115dbb7599.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d009245c3d17194db301ee4b34f0e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa5f28bec739b0b2e906",
+    "url": "/static/css/2.509bb6f6.chunk.css"
+  },
+  {
+    "revision": "6c033a31413bb7b53d9f",
+    "url": "/static/css/main.1d01cf78.chunk.css"
+  },
+  {
+    "revision": "fa5f28bec739b0b2e906",
+    "url": "/static/js/2.c6c03918.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c6c03918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c033a31413bb7b53d9f",
+    "url": "/static/js/main.f0033500.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "20da5085ab06d524caf6608f97e3352d",
+    "url": "/static/media/background.20da5085.png"
+  },
+  {
+    "revision": "d8a4ee768f972a4b5b008ba2ec7b3f27",
+    "url": "/static/media/image-placeholder.d8a4ee76.webp"
+  }
+]);
